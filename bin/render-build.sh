8837d5c35bfbbed6c3e3e672ac5bb385efafdb11
Alexey Cherebayev <alexey.cherebayev@tele2.kz>
2024-11-14 13:04:00 +0500

    Added render-build.sh file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,6 @@
 set -o errexit
 
 bundle install
-RAILS_ENV=production bundle exec rake assets:precompile --trace
-RAILS_ENV=production bundle exec rake assets:precompile --trace
 bundle exec rake db:migrate
 
 
